5c04d5bd450a033c41c1d6c33c36f522b2efc008
tdviri <tdviri20@gmail.com>
2022-01-28 15:38:54 -0500

    Implemented RecyclerView

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -2,15 +2,25 @@ package com.example.simpletodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    val listOfTasks = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //1. Let's detect when the user clicks on the add button
-        findViewById<Button>(R.id.button)
+//        findViewById<Button>(R.id.button).setOnClickListener {
+//            //Code in here is going to be executed when the user clicks on a button
+//            Log.i("Caren", "User clicked on button")
+//        }
+
+        listOfTasks.add("Do laundry")
+        listOfTasks.add("Go for a walk")
     }
 }
 
